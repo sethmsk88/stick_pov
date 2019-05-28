302983e12e0e1c7082e4302f8a71188d0fd5ab7c
Seth Kerr <sethmsk88@yahoo.com>
2019-05-28 19:36:23 -0400

    Create class for new remote control

---
+++ RemoteControlRoku.cpp
@@ -0,0 +1,49 @@
+typedef unsigned long uint32_t;
+
+class RemoteControlRoku
+{
+    public:
+        // Button IR Codes
+        static const uint32_t BTN_POWER = 1474553879;
+        static const uint32_t BTN_BACK = 1474520729;
+        static const uint32_t BTN_HOME = 1474543679;
+        static const uint32_t BTN_UP = 1474533479;
+        static const uint32_t BTN_DOWN = 1474546739;
+        static const uint32_t BTN_LEFT = 1474525319;
+        static const uint32_t BTN_RIGHT = 1474540619;
+        static const uint32_t BTN_OK = 1474516139;
+        static const uint32_t BTN_RETURN = 1474502369;
+        static const uint32_t BTN_ASTERISK = 1474528889;
+        static const uint32_t BTN_REWIND = 1474505939;
+        static const uint32_t BTN_PLAYPAUSE = 1474507469;
+        static const uint32_t BTN_FASTFORWARD = 1474538069;
+        static const uint32_t BTN_MEDIA_0 = 1474513589;
+        static const uint32_t BTN_MEDIA_1 = 1474548269;
+        static const uint32_t BTN_MEDIA_2 = 1474559489;
+        static const uint32_t BTN_MEDIA_3 = 1474498799;
+        static const uint32_t BTN_VOL_UP = 1474555919;
+        static const uint32_t BTN_VOL_DOWN = 1474496759;
+        static const uint32_t BTN_MUTE = 1474495739;
+
+        // Holding Button IR Codes
+        static const uint32_t BTN_POWER_HOLD = 1474554134;
+        static const uint32_t BTN_BACK_HOLD = 1474520984;
+        static const uint32_t BTN_HOME_HOLD = 1474543934;
+        static const uint32_t BTN_UP_HOLD = 1474533734;
+        static const uint32_t BTN_DOWN_HOLD = 1474546994;
+        static const uint32_t BTN_LEFT_HOLD = 1474525574;
+        static const uint32_t BTN_RIGHT_HOLD = 1474540874;
+        static const uint32_t BTN_OK_HOLD = 1474516394;
+        static const uint32_t BTN_RETURN_HOLD = 1474502624;
+        static const uint32_t BTN_ASTERISK_HOLD = 1474529144;
+        static const uint32_t BTN_REWIND_HOLD = 1474506194;
+        static const uint32_t BTN_PLAYPAUSE_HOLD = 1474507724;
+        static const uint32_t BTN_FASTFORWARD_HOLD = 1474538324;
+        static const uint32_t BTN_MEDIA_0_HOLD = 1474513844;
+        static const uint32_t BTN_MEDIA_1_HOLD = 1474548524;
+        static const uint32_t BTN_MEDIA_2_HOLD = 1474559744;
+        static const uint32_t BTN_MEDIA_3_HOLD = 1474499054;
+        static const uint32_t BTN_VOL_UP_HOLD = 1474556174;
+        static const uint32_t BTN_VOL_DOWN_HOLD = 1474497014;
+        static const uint32_t BTN_MUTE_HOLD = 1474495994;
+};
