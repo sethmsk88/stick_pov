c621c7f7e0960e7ecfc535c007efdb83264216ab
Seth Kerr <sethmsk88@yahoo.com>
2019-05-29 21:43:59 -0400

    Add button description function for debugging

--- RemoteControlRoku.cpp
+++ RemoteControlRoku.cpp
@@ -1,3 +1,4 @@
+#include <WString.h>
 typedef unsigned long uint32_t;
 
 class RemoteControlRoku
@@ -46,4 +47,90 @@ class RemoteControlRoku
         static const uint32_t BTN_VOL_UP_HOLD = 1474556174;
         static const uint32_t BTN_VOL_DOWN_HOLD = 1474497014;
         static const uint32_t BTN_MUTE_HOLD = 1474495994;
+
+        static String RemoteControlRoku::getBtnDescription(uint32_t btnCode) {
+          switch(btnCode) {
+              case RemoteControlRoku::BTN_ASTERISK:
+                  return "Asterisk";
+              case RemoteControlRoku::BTN_ASTERISK_HOLD:
+                  return "Asterisk Hold";
+              case RemoteControlRoku::BTN_BACK:
+                  return "Back";
+              case RemoteControlRoku::BTN_BACK_HOLD:
+                  return "Back Hold";
+              case RemoteControlRoku::BTN_DOWN:
+                  return "Down";
+              case RemoteControlRoku::BTN_DOWN_HOLD:
+                  return "Down Hold";
+              case RemoteControlRoku::BTN_FASTFORWARD:
+                  return "Fast Forward";
+              case RemoteControlRoku::BTN_FASTFORWARD_HOLD:
+                  return "Fast Forward Hold";
+              case RemoteControlRoku::BTN_HOME:
+                  return "Home";
+              case RemoteControlRoku::BTN_HOME_HOLD:
+                  return "Home Hold";
+              case RemoteControlRoku::BTN_LEFT:
+                  return "Left";
+              case RemoteControlRoku::BTN_LEFT_HOLD:
+                  return "Left Hold";
+              case RemoteControlRoku::BTN_MEDIA_0:
+                  return "Media 0";
+              case RemoteControlRoku::BTN_MEDIA_0_HOLD:
+                  return "Media 0 Hold";
+              case RemoteControlRoku::BTN_MEDIA_1:
+                  return "Media 1";
+              case RemoteControlRoku::BTN_MEDIA_1_HOLD:
+                  return "Media 1 Hold";
+              case RemoteControlRoku::BTN_MEDIA_2:
+                  return "Media 2";
+              case RemoteControlRoku::BTN_MEDIA_2_HOLD:
+                  return "Media 2 Hold";
+              case RemoteControlRoku::BTN_MEDIA_3:
+                  return "Media 3";
+              case RemoteControlRoku::BTN_MEDIA_3_HOLD:
+                  return "Media 3 Hold";
+              case RemoteControlRoku::BTN_MUTE:
+                  return "Mute";
+              case RemoteControlRoku::BTN_MUTE_HOLD:
+                  return "Mute Hold";
+              case RemoteControlRoku::BTN_OK:
+                  return "OK";
+              case RemoteControlRoku::BTN_OK_HOLD:
+                  return "OK Hold";
+              case RemoteControlRoku::BTN_PLAYPAUSE:
+                  return "Play/Pause";
+              case RemoteControlRoku::BTN_PLAYPAUSE_HOLD:
+                  return "Play/Pause Hold";
+              case RemoteControlRoku::BTN_POWER:
+                  return "Power";
+              case RemoteControlRoku::BTN_POWER_HOLD:
+                  return "Power Hold";
+              case RemoteControlRoku::BTN_RETURN:
+                  return "Return";
+              case RemoteControlRoku::BTN_RETURN_HOLD:
+                  return "Return Hold";
+              case RemoteControlRoku::BTN_REWIND:
+                  return "Rewind";
+              case RemoteControlRoku::BTN_REWIND_HOLD:
+                  return "Rewind Hold";
+              case RemoteControlRoku::BTN_RIGHT:
+                  return "Right";
+              case RemoteControlRoku::BTN_RIGHT_HOLD:
+                  return "Right Hold";
+              case RemoteControlRoku::BTN_UP:
+                  return "Up";
+              case RemoteControlRoku::BTN_UP_HOLD:
+                  return "Up Hold";
+              case RemoteControlRoku::BTN_VOL_DOWN:
+                  return "Vol Down";
+              case RemoteControlRoku::BTN_VOL_DOWN_HOLD:
+                  return "Vol Down Hold";
+              case RemoteControlRoku::BTN_VOL_UP:
+                  return "Vol Up";
+              case RemoteControlRoku::BTN_VOL_UP_HOLD:
+                  return "Vol Up Hold";
+          }
+          return "";
+      }
 };
